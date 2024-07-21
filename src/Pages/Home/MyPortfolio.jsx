d21d9e39957dc063c673648fa265daa6e62cdf2d
Jayesh-Singh-Lodha <jayeshsinghlodha.cse25@jecrc.ac.in>
2024-07-21 17:43:22 +0530

    web links added

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -42,7 +42,6 @@ export default function MyPortfolio() {
             <div className="portfolio--section--card--content">
               <div>
                 <h3 className="portfolio--section--title">{item.title}:</h3>
-
                 <h3 className="item--description--title">Platform:
                   <span> {item.description.Platform}</span>
                 </h3>
@@ -61,10 +60,11 @@ export default function MyPortfolio() {
                   })}
                 </ul>
               </div>
-              <a href={item.projectLink}
+              <div className="portfolio--section--links">
+              <a href={item.websiteLink}
               style={{'textDecoration':'none'}}>
                 <p className="text-sm portfolio--link">
-                  {item.link}
+                  Website
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
@@ -82,6 +82,28 @@ export default function MyPortfolio() {
                   </svg>
                 </p>
               </a>
+              <a href={item.gitHubLink}
+              style={{'textDecoration':'none'}}>
+                <p className="text-sm portfolio--link">
+                  GitHub
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 20 19"
+                    fill="none"
+                  >
+                    <path
+                      d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                      stroke="currentColor"
+                      stroke-width="2.66667"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </p>
+              </a>
+              </div>
             </div>
           </div>
         ))}
